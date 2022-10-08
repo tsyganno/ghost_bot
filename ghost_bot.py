36f99625fe510f5cbddbbdd23bae39c2f04e98cc
tsyganno <tsyganlee@gmail.com>
2022-10-08 19:59:29 +0800

    second

--- ghost_bot.py
+++ ghost_bot.py
@@ -1,1 +1,20 @@
+import logging
+from aiogram import Bot, Dispatcher, types, executor
 
+
+API_TOKEN = 'key'
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        'Ты попал в капкан!'
+    )
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
